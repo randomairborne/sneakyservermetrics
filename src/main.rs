168a0551d75a9278ce82afd3db0574dab14760b1
valkyrie_pilot <valk@randomairborne.dev>
2023-12-01 00:52:34 -0700

    axum 0.7, misc changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{sync::Arc, time::Duration};
+use std::{net::IpAddr, sync::Arc, time::Duration};
 
 use axum::{extract::State, routing::get};
 use prometheus::{Encoder, IntGauge, Registry, TextEncoder};
 use reqwest::Client;
-use tokio::{select, sync::watch::Receiver, time::MissedTickBehavior};
+use tokio::{net::TcpListener, time::MissedTickBehavior};
 
 const LINK: &str = "https://discord.com/api/v10/invites/minecraft?with_counts=true";
 
@@ -20,7 +20,6 @@ async fn main() {
     reg.register(Box::new(boosts.clone())).unwrap();
 
     let state = Arc::new(reg);
-    let (send, mut recv) = tokio::sync::watch::channel::<()>(());
     tokio::spawn(event_loop(
         Gauges {
             members,
@@ -28,31 +27,15 @@ async fn main() {
             boosts,
         },
         client,
-        recv.clone(),
     ));
-    tokio::spawn(async move {
-        use tokio::signal::unix::{signal, SignalKind};
-        let mut quit = signal(SignalKind::quit()).unwrap();
-        let mut terminate = signal(SignalKind::terminate()).unwrap();
-        let mut interrupt = signal(SignalKind::interrupt()).unwrap();
-        select! {
-            _ = quit.recv() => {},
-            _ = terminate.recv() => {},
-            _ = interrupt.recv() => {},
-        }
-        send.send(()).unwrap();
-    });
-    let router = axum::Router::new()
+    let app = axum::Router::new()
         .route("/metrics", get(metrics))
         .with_state(state);
-    println!("Starting server on 0.0.0.0:9000");
-    axum::Server::bind(&([0, 0, 0, 0], 9000).into())
-        .serve(router.into_make_service())
-        .with_graceful_shutdown(async move {
-            recv.changed().await.unwrap();
-        })
+    let tcp = TcpListener::bind((IpAddr::from([0, 0, 0, 0]), 9000))
         .await
         .unwrap();
+    println!("Starting server on {}", tcp.local_addr().unwrap());
+    axum::serve(tcp, app).await.unwrap();
 }
 
 pub async fn metrics(State(reg): State<Arc<Registry>>) -> Vec<u8> {
@@ -63,14 +46,11 @@ pub async fn metrics(State(reg): State<Arc<Registry>>) -> Vec<u8> {
     buffer
 }
 
-pub async fn event_loop(gauges: Gauges, client: Client, mut shutdown: Receiver<()>) {
+pub async fn event_loop(gauges: Gauges, client: Client) {
     let mut interval = tokio::time::interval(Duration::from_secs(15));
     interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
     loop {
-        select! {
-            _ = shutdown.changed() => {},
-            _ = interval.tick() => {}
-        }
+        interval.tick().await;
         let req = match client.get(LINK).send().await {
             Ok(v) => v,
             Err(e) => {
